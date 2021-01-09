9e506b7a4e0dab4ccf59f0d327506260ceb43529
sua1009 <sua100913@gmail.com>
2021-01-09 13:20:54 +0900

    추상클래스 ImplClass를 상속받아 method3을 구현

---
+++ src/ch08/ImplClassChild.java
@@ -0,0 +1,10 @@
+package ch08;
+
+public class ImplClassChild extends ImplClass {
+
+	@Override
+	public void method3() {
+		// TODO Auto-generated method stub
+		System.out.println("ImplClass 에서 상속받은 추상메서드 method3 구현");
+	}
+}
