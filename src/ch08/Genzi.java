124d72597a82120448953dd7d2eb9bdd258a8c05
sua1009 <sua100913@gmail.com>
2021-01-09 13:19:34 +0900

    interface로 구현된 Character를 상속받아 구현한 클래스

---
+++ src/ch08/Genzi.java
@@ -0,0 +1,28 @@
+package ch08;
+
+public class Genzi implements Character {
+
+	@Override
+	public void move() {
+		System.out.println("겐지가 벽타고 이동합니다.");
+	}
+
+	@Override
+	public void attack() {
+		System.out.println("겐지가 수리검으로 공격합니다.");
+
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("겐지가 멈춥니다.");
+
+	}
+
+	@Override
+	public void skill() {
+		System.out.println("겐지가 용검을 사용합니다.");
+
+	}
+
+}
