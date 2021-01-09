bdf4c409ce26cde758e3b63c508bb8564c0e0937
sua1009 <sua100913@gmail.com>
2021-01-09 14:16:37 +0900

    Movement, Attack 인터페이스를 다중 상속받은 Character2 인터페이스를 구현한 Sombra 클래스.

---
+++ src/ch08/Sombra.java
@@ -0,0 +1,45 @@
+package ch08;
+
+public class Sombra implements Character2 {
+
+	@Override
+	public void left() {
+		System.out.println("솜브라가 왼쪽으로 이동합니다.");
+	}
+
+	@Override
+	public void right() {
+		System.out.println("솜브라가 왼쪽으로 이동합니다.");
+		
+	}
+
+	@Override
+	public void forward() {
+		System.out.println("솜브라가 왼쪽으로 이동합니다.");
+		
+	}
+
+	@Override
+	public void backward() {
+		System.out.println("솜브라가 왼쪽으로 이동합니다.");
+		
+	}
+
+	@Override
+	public void attack() {
+		System.out.println("솜브라가 왼쪽으로 이동합니다.");
+	
+	}
+
+	@Override
+	public void skill() {
+		System.out.println("솜브라가 왼쪽으로 이동합니다.");
+	
+	}
+
+	@Override
+	public void specialSkill() {
+		System.out.println("솜브라가 왼쪽으로 이동합니다.");		
+	}
+
+}
