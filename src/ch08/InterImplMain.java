b457bb59e298b2d1617e4cbd72030654c356c609
sua1009 <sua100913@gmail.com>
2021-01-09 13:44:28 +0900

    ImplClassChild 클래스를 객체화하여 사용

---
+++ src/ch08/InterImplMain.java
@@ -0,0 +1,15 @@
+package ch08;
+
+public class InterImplMain {
+
+	public static void main(String[] args) {
+		
+		ImplClassChild implChild = new ImplClassChild();
+		implChild.method1();
+		implChild.method2();
+		implChild.method3();
+	
+
+	}
+
+}
