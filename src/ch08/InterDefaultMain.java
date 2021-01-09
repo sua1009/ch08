664c07995a1d9c99bf851a5bcbdb2bd2400e3ae7
sua1009 <sua100913@gmail.com>
2021-01-09 13:43:58 +0900

    인터페이스의 추상메서드와 디폴트메서드의 차이를 확인하기 위한 실행 클래스

---
+++ src/ch08/InterDefaultMain.java
@@ -0,0 +1,14 @@
+package ch08;
+
+public class InterDefaultMain {
+
+	public static void main(String[] args) {
+		InterDefaultImpl idi = new InterDefaultImpl(); 
+
+		idi.method1();
+		idi.method2();
+		idi.InterDefaultmethod();
+		
+	}
+
+}
