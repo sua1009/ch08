1278284c9d03f2c2dc97abb390472a599d212b2e
sua1009 <sua100913@gmail.com>
2021-01-09 14:03:33 +0900

    디폴트 메서드를 활용하여 기능을 확장하는 것을 실행하는 클래스

---
+++ src/ch08/MyDefaultMethodMain.java
@@ -0,0 +1,17 @@
+package ch08;
+
+public class MyDefaultMethodMain {
+
+	public static void main(String[] args) {
+		MyInterface mi1 = new MyClassA();
+		mi1.method1();
+		mi1.method2();
+		
+		MyInterface mi2 = new MyClassB();
+		mi2.method1();
+		mi2.method2();
+		
+
+	}
+
+}
